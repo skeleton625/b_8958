ea8e93837da5427d576234501e7347a0379e7dfc
WaniFi <skelpd24@gmail.com>
2018-08-29 12:35:00 +0900

    8958 문제 추가 시간 보완

--- baekjoon_8958/main.cpp
+++ baekjoon_8958/main.cpp
@@ -1,18 +1,17 @@
-#include <iostream>
-using namespace std;
+#include <cstdio>
 
 int t, c, m;
 char i1[81];
 int main() {
-	cin >> t;
+	scanf_s("%d", &t);
 	while (t--) {
-		cin >> i1;
+		scanf_s("%s", i1, 81);
 		for (int i = 0; i1[i]; i++) {
 			if (i1[i] == 'X') c = 0;
 			else c++;
 			m += c;
 		}
-		cout << m << endl;
+		printf("%d\n",m);
 		m = 0; c = 0;
 	}
 	return 0;
